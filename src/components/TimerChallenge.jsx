66ac5037eb1c4836242fbfb82f66335f6d2b2bd9
rzzza_rehan <rhn.raza121@gmail.com>
2024-09-05 00:45:09 +0530

    initial commit

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,17 @@
+export default function TimerChallenge({ title, targetTime }) {
+    return (
+        <section className="challenge">
+            <h2>{title}</h2>
+            <p className="challenge-time">
+                {targetTime} second{targetTime>1?'s':''}
+            </p>
+            <p>
+                <button>Start Chanllenge</button>
+            </p>
+            <p>
+                Timmer is running... / Timer inactive
+            </p>
+        </section>
+    )
+
+}
