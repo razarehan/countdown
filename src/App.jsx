13a27ba96fabebcfeca89ad8a8efc87b1b7f7b2e
rzzza_rehan <rhn.raza121@gmail.com>
2024-09-06 23:08:09 +0530

    create resultModal comp. and use useImperativeHandle hook to exposing a dialog ref handle to the TimeChallenge component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges"></div>
+      <Player/>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}/>
+        <TimerChallenge title="Not easy" targetTime={5}/>
+        <TimerChallenge title="Medium" targetTime={10}/>
+        <TimerChallenge title="Pros only" targetTime={15}/>
+      </div>
     </>
   );
 }
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,36 @@
+import { useState, useRef } from "react"
+import ResultModal from './ResultModal.jsx';
+
 export default function TimerChallenge({ title, targetTime }) {
-    return (
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+    const timer = useRef();
+    const dialog = useRef();
+    function handleStart() {
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+            dialog.current.open();
+        }, targetTime * 1000);
+        
+        setTimerStarted(true);
+    }
+    function handleStop() {
+        clearTimeout(timer.current)
+    }
+    return <>
+        <ResultModal ref={dialog} result="Lost" targetTime={1}/>
         <section className="challenge">
             <h2>{title}</h2>
             <p className="challenge-time">
                 {targetTime} second{targetTime>1?'s':''}
             </p>
             <p>
-                <button>Start Chanllenge</button>
+                <button onClick={!timerStarted?handleStart:handleStop}>
+                    {!timerStarted?'Start':'Stop'} Chanllenge</button>
             </p>
-            <p>
-                Timmer is running... / Timer inactive
+            <p className={timerStarted?'active':''}>
+                {timerStarted?'Timmer is running...': 'Timer inactive'}
             </p>
         </section>
-    )
-
+    </>
 }
